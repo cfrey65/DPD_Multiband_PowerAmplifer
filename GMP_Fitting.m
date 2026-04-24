5597d55cbb6c6c4a04422b84a5096157696f3d66
cfrey65 <129206827+cfrey65@users.noreply.github.com>
2026-04-24 12:28:50 -0400

    GMP_Fitting.m

---
+++ GMP_Fitting.m
@@ -0,0 +1,30 @@
+deg = 2;
+mem = 2;
+l = 2;
+
+
+coeff = [];
+RMSin = -20.5;
+y = RFWebLab_IMS2026_PA_meas_v1_1(x(1:1000), RMSin);
+
+for i = 1:(10+mem+l+1)
+    row = [];
+    for m = 1:mem
+        for d = 1:deg
+            inp = i-m;
+            row(end+1) = y(inp+mem+)*abs(y(inp))^d;
+            for j = 1:l 
+              row(end+1) = y(inp)*abs(y(inp-j))^d;
+              row(end+1) = y(inp)*abs(y(inp+j))^d;
+            end
+        end
+    end
+    coeff = [coeff; row];
+
+end
+coeff
+pa = resize(y(1:10), [10, 1]);
+size(y(1:10))
+size(coeff)
+targ = pa\coeff
+%DO gmp/target to calculate coeff
